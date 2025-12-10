be36aff821bb7549fa05e6ff9e19ea5bcb57f1be
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-10 22:21:54 +0530

    Improved Tab bar navigation
    - Added unique icons for each tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,86 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+interface TabBarIconProps {
+  icon: string;
+  focused: boolean;
+  color: string;
+  size: number;
+}
+
+const TabBarIcon = ({ icon, focused, color, size }: TabBarIconProps) => {
+  return (
+    <Ionicons
+      name={focused ? (`${icon}-sharp` as any) : (`${icon}-outline` as any)}
+      size={size}
+      color={color}
+    />
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="StatsScreen" />
-      <Tabs.Screen name="AddExpense" />
-      <Tabs.Screen name="ProfileScreen" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#549994",
+        tabBarInactiveTintColor: "#aaaaaa",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <TabBarIcon
+              icon="home"
+              focused={focused}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="StatsScreen"
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <TabBarIcon
+              icon="bar-chart"
+              focused={focused}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="AddExpense"
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <TabBarIcon
+              icon="add-circle"
+              focused={focused}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ProfileScreen"
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <TabBarIcon
+              icon="person-circle"
+              focused={focused}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
