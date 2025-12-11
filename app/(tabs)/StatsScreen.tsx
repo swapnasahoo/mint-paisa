50a4966416eab4ca240f24475279b5fc56ae9ae0
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-11 21:30:53 +0530

    Added StatsScreen UI
    - Added basic structure for statistics

--- app/(tabs)/StatsScreen.tsx
+++ app/(tabs)/StatsScreen.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StatusBar } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
 const StatsScreen = () => {
-  return (
-    <View>
-      <Text>StatsScreen</Text>
-    </View>
-  )
-}
+  StatusBar.setBarStyle("dark-content");
 
-export default StatsScreen
+  return (
+    <View className="flex-1 bg-neutral-50 px-6 py-4">
+      <SafeAreaView className="flex-1">
+        {/* HEADER */}
+        <View className="flex-row items-center justify-between">
+          <Ionicons name="arrow-back" size={24} color="black" />
+
+          <Text className="text-black text-lg font-semibold">Statistics</Text>
+
+          <Ionicons name="download-outline" size={24} color="black" />
+        </View>
+
+        {/* SUMMARY */}
+        <View className="mt-6">
+          <Text className="text-2xl text-black font-bold">Balance: ₹500</Text>
+          <Text className="text-lg text-black font-semibold">
+            Income: ₹1000.00
+          </Text>
+          <Text className="text-lg text-black font-semibold">
+            Expense: ₹500.00
+          </Text>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default StatsScreen;
