d0731d2d41199034b0ebcea6a418b6bf21eabd06
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-11 07:47:35 +0530

    Added Home screen UI
    - Added mock greetings and username
    - Added mock notification icon
    - Added mock transaction summary card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StatusBar, Pressable } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
 const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+  StatusBar.setBarStyle("light-content");
 
-export default index
+  const [isTotalVisible, setIsTotalVisible] = useState<boolean>(true);
+
+  return (
+    <View className="flex-1 bg-neutral-50">
+      <View className="w-full h-[50%] bg-[#429690] px-6 py-4 rounded-b-2xl">
+        <SafeAreaView className="flex-1 overflow-hidden">
+          <View className="flex-row items-center">
+            <View>
+              <Text className="text-white text-sm ">Good afternoon!</Text>
+              <Text className="text-white font-semibold text-xl">
+                SwapnaSahoo
+              </Text>
+            </View>
+
+            <Pressable className="ml-auto bg-white/15 p-2 rounded-lg transition-all duration-300 ease-in-out active:opacity-75 active:scale-[0.98]">
+              <Ionicons name="notifications-outline" size={24} color="white" />
+
+              {/* MOCK NOTIFICATION INDICATION */}
+              <View className="size-2 bg-[#FFAB7B] rounded-full absolute top-3 right-3" />
+            </Pressable>
+          </View>
+
+          {/* TRANSACTION SUMMARY CARD */}
+          <View className="w-full h-60 bg-[#2F7E79] z-10 mx-auto top-40 rounded-2xl px-6 py-4 shadow-lg elevation-lg">
+            {/* TOTAL BALANCE */}
+            <View>
+              <Pressable
+                className="flex-row items-center gap-2"
+                onPress={() => setIsTotalVisible(!isTotalVisible)}
+              >
+                <Text className="text-white text-lg font-semibold">
+                  Total Balance
+                </Text>
+                <Ionicons
+                  name={isTotalVisible ? "chevron-up" : "chevron-down"}
+                  size={24}
+                  color="white"
+                />
+              </Pressable>
+
+              {/* BALANCE */}
+              <Text
+                className={`text-white text-3xl font-bold ${isTotalVisible ? "block" : "hidden"}`}
+              >
+                ₹500.00
+              </Text>
+            </View>
+
+            {/* INCOME + EXPENSE SUMMARY */}
+            <View className="flex-row items-center justify-between mt-auto">
+              {/* INCOME */}
+              <View>
+                <View className="flex-row items-center gap-2">
+                  <View className="bg-white/15 p-1 rounded-full">
+                    <Ionicons name="arrow-down" size={18} color="white" />
+                  </View>
+                  <Text className="text-[#D0E5E4] font-medium">Income</Text>
+                </View>
+                {/* INCOME BALANCE */}
+                <Text className="text-xl text-white font-semibold text-right">
+                  ₹1000.00
+                </Text>
+              </View>
+
+              {/* EXPENSE */}
+              <View>
+                <View className="flex-row items-center gap-2">
+                  <View className="bg-white/15 p-1 rounded-full">
+                    <Ionicons name="arrow-up" size={18} color="white" />
+                  </View>
+                  <Text className="text-[#D0E5E4] font-medium">Expense</Text>
+                </View>
+                {/* INCOME BALANCE */}
+                <Text className="text-xl text-white font-semibold text-right">
+                  ₹500.00
+                </Text>
+              </View>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
+    </View>
+  );
+};
+
+export default index;
