1d1640907f4c08a7445e07f6ef8e818f0a09328d
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-15 00:55:18 +0530

    Added Donut chart into StatsScreen component
    - Removed example stats content
    - Added react-native-gifted-charts and used its donut chart
    - Fetch balances from dB and displayed in the chart

--- app/(tabs)/StatsScreen.tsx
+++ app/(tabs)/StatsScreen.tsx
@@ -1,10 +1,78 @@
-import { View, Text, StatusBar } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { account } from "@/libs/appwrite";
+import { fetchTransactions } from "@/services/transaction.service";
 import { Ionicons } from "@expo/vector-icons";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { StatusBar, Text, View } from "react-native";
+import { PieChart, pieDataItem } from "react-native-gifted-charts";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const StatsScreen = () => {
   StatusBar.setBarStyle("dark-content");
+
+  const [userId, setUserId] = useState<string>("");
+  const [totalBalance, setTotalBalance] = useState<number>(0);
+  const [totalIncome, setTotalIncome] = useState<number>(0);
+  const [totalExpense, setTotalExpense] = useState<number>(0);
+
+  const [focusedIndex, setFocusedIndex] = useState<number | null>(null);
+
+  const donutChartData: pieDataItem[] = [
+    {
+      value: totalIncome,
+      color: "#3CB371",
+      onPress: () => setFocusedIndex(0),
+    },
+    {
+      value: totalExpense,
+      color: "#F95B51",
+      onPress: () => setFocusedIndex(1),
+    },
+  ];
+
+  // LOAD USER ID
+  useEffect(() => {
+    async function loadUserId() {
+      const user = await account.get();
+      setUserId(user.$id);
+    }
+    loadUserId();
+  }, []);
+
+  // FETCH BALANCES
+  useFocusEffect(
+    useCallback(() => {
+      let isActive: boolean = true;
+      async function fetchBalances() {
+        if (!userId) return;
+
+        const transaction = await fetchTransactions(userId);
+
+        const balance = transaction?.rows.reduce((acc, transaction) => {
+          return acc + transaction.amount;
+        }, 0);
+
+        const income = transaction?.rows.reduce((acc, transaction) => {
+          return transaction.type === "income" ? acc + transaction.amount : acc;
+        }, 0);
+
+        const expense = transaction?.rows.reduce((acc, transaction) => {
+          return transaction.type === "expense"
+            ? acc + transaction.amount
+            : acc;
+        }, 0);
+
+        setTotalIncome(income || 0);
+        setTotalExpense(expense || 0);
+        setTotalBalance(balance || 0);
+      }
+      isActive && fetchBalances();
+
+      return () => {
+        isActive = false;
+      };
+    }, [userId])
+  );
 
   return (
     <View className="flex-1 bg-neutral-50 px-6 py-4">
@@ -18,15 +86,29 @@ const StatsScreen = () => {
           <Ionicons name="download-outline" size={24} color="black" />
         </View>
 
-        {/* SUMMARY */}
+        {/* STATS CONTENT */}
         <View className="mt-6">
-          <Text className="text-2xl text-black font-bold">Balance: ₹500</Text>
-          <Text className="text-lg text-black font-semibold">
-            Income: ₹1000.00
-          </Text>
-          <Text className="text-lg text-black font-semibold">
-            Expense: ₹500.00
-          </Text>
+          {/* INCOME VS EXPENSE CHART */}
+          <View className="mx-auto">
+            <PieChart
+              data={donutChartData}
+              donut
+              innerRadius={70}
+              radius={100}
+              focusOnPress
+              centerLabelComponent={() => {
+                return (
+                  <Text className="text-2xl font-medium text-gray-800 text-center">
+                    {focusedIndex === null
+                      ? `Total\n${totalBalance}`
+                      : focusedIndex === 0
+                      ? `${((totalIncome / totalBalance) * 100).toFixed(2)}%`
+                      : `${((totalExpense / totalBalance) * 100).toFixed(2)}%`}
+                  </Text>
+                );
+              }}
+            />
+          </View>
         </View>
       </SafeAreaView>
     </View>
