ec70880b781b2088124c120cc76f7f26903214af
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-20 12:18:58 +0530

    Improved Transaction Card
    - Added a background to it
    - Added on active effect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -163,9 +163,10 @@ const index = () => {
           keyExtractor={(item) => item.$id}
           showsVerticalScrollIndicator={false}
           className="mt-4"
+          contentContainerStyle={{ paddingHorizontal: 2 }}
           renderItem={({ item }) => (
             /* TRANSACTION CARD */
-            <View className="flex-row items-center gap-3 mt-2 mb-2">
+            <View className="flex-row items-center gap-3 mt-2 mb-2 bg-neutral-50 px-4 py-3 shadow-sm elevation-sm rounded-lg transition-all ease-in-out duration-300 active:opacity-75 active:scale-[0.98]">
               {/* LOGO / CATEGORY ICON */}
               <View className="bg-neutral-200/50 border border-neutral-200/60 p-2 rounded-lg items-center justify-center">
                 <Image
