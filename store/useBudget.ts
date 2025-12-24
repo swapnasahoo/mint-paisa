f0acc567a4abfc4a42d01a9e274b429cc8c3cdb1
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-24 13:21:19 +0530

    Change budget type from number to string in useBudget store

--- store/useBudget.ts
+++ store/useBudget.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
 type BudgetType = {
-  budget: number;
-  setBudget: (budget: number) => void;
+  budget: string;
+  setBudget: (budget: string) => void;
 };
 
 export const useBudget = create<BudgetType>((set) => ({
-  budget: 0,
+  budget: "",
   setBudget: (budget) => set({ budget }),
 }));
