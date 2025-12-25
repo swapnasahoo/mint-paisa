18afd46455b37ecfd08977850b2e07ba0bbd45cc
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-25 21:43:53 +0530

    Add the logic to get the requested transaction from list

--- app/transaction/[transactionId].tsx
+++ app/transaction/[transactionId].tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { TransactionRow } from "@/interfaces/TransactionRow";
+import { useTransactions } from "@/store/useTransaction";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 const TransactionDetails = () => {
+  const { transactionId } = useLocalSearchParams();
+
+  const transactions = useTransactions((s) => s.transactions);
+  const [transaction, setTransaction] = useState<TransactionRow | null>(null);
+
+  useEffect(() => {
+    if (!transactionId) return;
+
+    const transactionData = transactions.find((t) => t.$id === transactionId);
+    setTransaction(transactionData || null);
+  }, [transactionId]);
+
   return (
     <View>
       <Text>TransactionDetails</Text>
