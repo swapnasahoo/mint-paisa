500cf64bf97ac82ac9ae1f3c83bcdaa24bf81bed
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-12-26 15:01:58 +0530

    Add update transaction modal UI

--- app/transaction/[transactionId].tsx
+++ app/transaction/[transactionId].tsx
@@ -4,23 +4,42 @@ import showToast from "@/libs/showToast";
 import { deleteTransaction } from "@/services/transaction.service";
 import { useTransactions } from "@/store/useTransaction";
 import { Ionicons } from "@expo/vector-icons";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { router, useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Alert, Image, Pressable, Text, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const TransactionDetails = () => {
   const { transactionId } = useLocalSearchParams();
 
   const transactions = useTransactions((s) => s.transactions);
+  const [amount, setAmount] = useState<string>("");
+  const [date, setDate] = useState<Date>(new Date());
+  const [showDatePicker, setShowDatePicker] = useState<boolean>(false);
+
   const [transaction, setTransaction] = useState<TransactionRow | null>(null);
   const deleteTransactionStore = useTransactions((s) => s.deleteTransaction);
+
+  const [isUpdateModalVisible, setIsUpdateModalVisible] =
+    useState<boolean>(false);
 
   useEffect(() => {
     if (!transactionId) return;
 
     const transactionData = transactions.find((t) => t.$id === transactionId);
     setTransaction(transactionData || null);
+    setAmount(transactionData?.amount.toString() || "");
+    setDate(new Date(transactionData?.transactionDate || Date.now()));
   }, [transactionId]);
 
   async function handleDeleteTransaction() {
@@ -147,7 +166,7 @@ const TransactionDetails = () => {
           {/* EDIT TRANSACTION BUTTON */}
           <Pressable
             className="mt-6 px-6 py-4 bg-[#429690] rounded-lg shadow-sm elevation-xs transition-all ease-in-out duration-300 active:opacity-75 active:scale-[0.98]"
-            onPress={() => {}}
+            onPress={() => setIsUpdateModalVisible(true)}
           >
             <Text className="text-center text-white font-medium">
               Edit Transaction
@@ -155,8 +174,98 @@ const TransactionDetails = () => {
           </Pressable>
         </View>
       </SafeAreaView>
+
+      {/* UPDATE MODAL */}
+      <Modal visible={isUpdateModalVisible} transparent animationType="fade">
+        <View className="flex-1 justify-center items-center">
+          {/* OVERLAY */}
+          <Pressable
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+            onPress={() => setIsUpdateModalVisible(false)}
+          />
+
+          {/* MODAL CONTENT */}
+          <View className="bg-neutral-50 w-95 h-80 shadow-sm elevation-xs rounded-lg p-6">
+            <Text className="text-lg font-semibold mb-4">
+              Update Transaction
+            </Text>
+
+            {/* INPUT FIELDS */}
+            <View>
+              {/* AMOUNT INPUT */}
+              <View>
+                <Text className="text-sm text-[#666666] font-semibold tracking-wide">
+                  AMOUNT
+                </Text>
+                <TextInput
+                  placeholder="0"
+                  keyboardType="numeric"
+                  style={style.inputStyle}
+                  value={amount}
+                  onChangeText={(item) => {
+                    if (item.length > 10) return;
+
+                    const cleaned: string = item.replace(/[^0-9]/g, "");
+                    setAmount(cleaned);
+                  }}
+                />
+              </View>
+
+              {/* DATE INPUT */}
+              <View>
+                <Text className="text-sm text-[#666666] font-semibold tracking-wide">
+                  DATE
+                </Text>
+                <Pressable
+                  style={style.inputStyle}
+                  onPress={() => setShowDatePicker(true)}
+                >
+                  <Text>{date.toLocaleDateString("en-IN")}</Text>
+                </Pressable>
+
+                {showDatePicker && (
+                  <DateTimePicker
+                    value={date}
+                    display="spinner"
+                    onChange={(event, selectedDate) => {
+                      setDate(selectedDate || date);
+                      setShowDatePicker(false);
+                    }}
+                  />
+                )}
+              </View>
+            </View>
+
+            {/* UPDATE BUTTON */}
+            <View className="mt-4">
+              <Pressable
+                className="bg-[#69AEA9] px-6 py-2 rounded-md shadow-md elevation-sm transition-all duration-300 ease-in-out active:opacity-75 active:scale-[0.98]"
+                onPress={() => setIsUpdateModalVisible(false)}
+              >
+                <Text className="text-lg text-white font-medium text-center uppercase">
+                  Update
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: "#DDDDDD",
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 10,
+    marginTop: 6,
+  },
+});
 
 export default TransactionDetails;
